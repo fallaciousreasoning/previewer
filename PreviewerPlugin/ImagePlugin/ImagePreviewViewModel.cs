84b4d64b934893fe1db1d55cb97bdec0acb6a6a9
Jay Harris <jay.harris@outlook.co.nz>
2016-04-11 12:44:41 +0800

    Adds an image previewer

---
+++ PreviewerPlugin/ImagePlugin/ImagePreviewViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using PreviewerPlugins.Annotations;
+
+namespace PreviewerPlugins.ImagePlugin
+{
+    public class ImagePreviewViewModel : INotifyPropertyChanged
+    {
+        private string imageUri;
+
+        public string ImageUri
+        {
+            get { return imageUri; }
+            set
+            {
+                if (value == imageUri) return;
+                imageUri = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ PreviewerPlugin/ImagePlugin/ImagePreviewerPlugin.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace PreviewerPlugins.ImagePlugin
+{
+    public class ImagePreviewerPlugin : IFilePreviewer
+    {
+        private static readonly string[] SupportedTypes =
+        {
+            ".jpg",
+            ".jpeg",
+            ".png",
+            ".bmp",
+            ".gif",
+            ".tiff",
+            ".ico",
+        };
+
+        private ImagePreviewer previewer;
+
+        public string Name { get { return "Image Previewer"; } }
+        public string Author { get { return "Jay Harris"; } }
+        public string Description { get { return "A basic image previewer"; } }
+        public string Version { get { return "0"; } }
+
+        public bool CanPreview(string file)
+        {
+            var lower = file.ToLower();
+            return SupportedTypes.Any(lower.EndsWith);
+        }
+
+        public Control GetPreviewer(string file)
+        {
+            if (previewer == null)
+                previewer = new ImagePreviewer();
+
+            previewer.ViewModel.ImageUri = file;
+            return previewer;
+        }
+    }
+}
